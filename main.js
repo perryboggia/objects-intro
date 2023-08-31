75a469ca25035a54946f0579ddc7185547eb9862
Perry Boggia <perryboggia@gmail.com>
2023-08-31 10:38:18 -0400

    A descriptive message about what changes you made in this commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+/*
+    Basic object representing my dog Angus.
+    - The object itself is labeled with the variable of `dog`
+    - The integer of 1 is labeled with the key of `age`
+*/
+const angus = {
+    age: 1
+}
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+console.log(wardrobe.depth)
+wardrobe.material = "Cedar"
+
+console.log(wardrobe)
+
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+
+console.log(empireStateBuilding)
+git --version
+
